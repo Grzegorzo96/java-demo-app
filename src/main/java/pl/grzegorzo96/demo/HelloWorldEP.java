7af13eb3f1847ecaa98b1633c49de79770aa87b3
Grzegorz Osowski <grzegorz.osowski@asuri.pl>
2019-03-19 08:41:49 +0100

    changes in helloworldep

--- src/main/java/pl/grzegorzo96/demo/HelloWorldEP.java
+++ src/main/java/pl/grzegorzo96/demo/HelloWorldEP.java
@@ -11,6 +11,6 @@ public class HelloWorldEP {
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello")
     String hello(){
-        return "Hello World!";
+        return "Hello heroku World!";
     }
 }
